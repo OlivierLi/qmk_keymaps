2a88b4fa1cc3cd470774be55433048f710ba93cf
Olivier Li <olivierlidupuis@gmail.com>
2020-08-25 19:55:37 -0400

    Add comments

--- ergodox_ez/layer_with_mod_tap.h
+++ ergodox_ez/layer_with_mod_tap.h
@@ -8,38 +8,51 @@ static uint16_t ls_timer = 0;
 static bool layer_with_mod_in_progress = false;
 // ----------------------------------------------------------------------------
 
+// This function has to be called on process_record_user() to know if a layer
+// hold get interrupted. On interruption a tap is not triggered.
 void layer_with_mod_tap_on_key_press(uint16_t keycode){
   if(keycode != LAYER_TAP_MOD)
+    // Hold was interrupted, no longer in progress.
     layer_with_mod_in_progress = false;
 }
 
-void layer_with_mod_on_hold_key_on_tap(keyrecord_t *record, uint8_t layer, uint8_t holdMod, uint8_t keycode) {
+// Call this function to get a layer activation on hold with |hold_mod| active and |tap_keycode| on tap.
+// 
+// TODO: There should be a |layer_with_mod_in_progress| variable for each key that is bound to this function.
+// otherwise the tapping will be reactivated because another call to this function set it to true. This is only
+// a real problem if we start cording with keys that use this function.
+void layer_with_mod_on_hold_key_on_tap(keyrecord_t *record, uint8_t layer, uint8_t hold_mod, uint8_t tap_keycode) {
 
   // Key down.
   if (record->event.pressed) {
     ls_timer = timer_read();
 
-    // Activate the layer and modifier no matter what.
+    // Activate the layer and modifier first.
     layer_on(layer);
-    register_mods(MOD_BIT(holdMod));
+    register_mods(MOD_BIT(hold_mod));
     layer_with_mod_in_progress = true;
   }
   // Key up.
   else{
+
     // If tapping.
     if (timer_elapsed(ls_timer) < TAPPING_TERM) {
-      unregister_mods(MOD_BIT(holdMod));
+      // Reset state before the tap.
+      unregister_mods(MOD_BIT(hold_mod));
       layer_off(layer);
 
+      // If 
       if(layer_with_mod_in_progress)
-        tap_code(keycode);
+        tap_code(tap_keycode);
     }
     // If holding.
     else {
-      unregister_mods(MOD_BIT(holdMod));
+      // Reset state.
+      unregister_mods(MOD_BIT(hold_mod));
       layer_off(layer);
     }
 
+    // Key no longer held, no longer in progress.
     layer_with_mod_in_progress = false;
   }
 }
