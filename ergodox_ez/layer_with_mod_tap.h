c06fbdcbad8056c952cd94bc8e941cc4d25455ab
Olivier Li <olivierlidupuis@gmail.com>
2020-09-03 07:00:23 -0400

    cleanup

--- ergodox_ez/layer_with_mod_tap.h
+++ ergodox_ez/layer_with_mod_tap.h
@@ -54,10 +54,9 @@ void flush_pending(void){
 }
 
 // This function has to be called on process_record_user() to know if a layer
-// hold get interrupted. On interruption a tap is not triggered.
-// Returns true if the key was absorbed and should not buble up.
+// hold got interrupted.  Returns true if the press was absorbed and should not buble up.
 bool layer_with_mod_tap_on_key_press(bool is_down, uint16_t keycode){
-  // Any action on the layer tap mod key should be handled in the handler.
+  // Any action on the layer tap mod key should be handled in the layer_with_mod_on_hold_key_on_tap().
   if(keycode == LAYER_TAP_MOD){
     return false;
   }
@@ -71,9 +70,6 @@ bool layer_with_mod_tap_on_key_press(bool is_down, uint16_t keycode){
       interrupted = false;
       flush_pending();
   }
-
-  // if a key is tapped up here we can go back on the pending_keys and replace the associated down with KC_NO
-  // then we can tap the key?
 
   // If no more place to buffer keycodes. Just drop.
   if(pending_keys_count != PENDING_KEYS_BUFFER_SIZE-1){
@@ -89,18 +85,14 @@ bool layer_with_mod_tap_on_key_press(bool is_down, uint16_t keycode){
   return true;
 }
 
-// TODO: Support a tap modifier so we can use this with quotes
-// TODO: Find out why the quick :a example does not work in vim. What happens right now is that if LT, K, K , LT
-// happens all within TAPPING_TERM then we trigger the tapping option. We only want that when LT, LT, K, K happened during
-// the tapping term.
-
 // Call this function to get a layer activation on hold with |hold_mod| active and |tap_keycode| on tap.
 // 
 // TODO: There should be a |layer_tap_mod_in_progress| variable for each key that is bound to this function.
 // otherwise the tapping will be reactivated because another call to this function set it to true. This is only
 // a real problem if we start cording with keys that use this function.
+//
+// TODO: Support a tap modifier so we can use this with quotes
 void layer_with_mod_on_hold_key_on_tap(keyrecord_t *record, uint8_t layer, uint8_t hold_mod, uint8_t tap_keycode) {
-
   // Key down.
   if (record->event.pressed) {
     last_layer_tap_mod_down_time = timer_read();
@@ -114,10 +106,12 @@ void layer_with_mod_on_hold_key_on_tap(keyrecord_t *record, uint8_t layer, uint8
   // Key up.
   else{
     if (timer_elapsed(last_layer_tap_mod_down_time) < TAPPING_TERM) {
+      // Normal tap.
       if(!interrupted){
         // Reset state.
         unregister_mods(MOD_BIT(hold_mod));
         layer_off(layer);
+
         tap_code(tap_keycode);
 
         // Key no longer held, no longer in progress.
@@ -125,6 +119,7 @@ void layer_with_mod_on_hold_key_on_tap(keyrecord_t *record, uint8_t layer, uint8
         return;
       }
 
+      // A full key press happened within the tapping term.
       if(complete_press_buffered()){
         flush_pending();
 
@@ -138,6 +133,7 @@ void layer_with_mod_on_hold_key_on_tap(keyrecord_t *record, uint8_t layer, uint8
         layer_off(layer);
 
         tap_code(tap_keycode);
+
         flush_pending();
       }
     }
